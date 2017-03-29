79bb00a1e9a82ee1e28c6caccc9b6323c22e34c7
hamad Khawaja <khawajahamad217@gmail.com>
2017-03-28 23:13:17 -0400

    Initial commit

---
+++ Foodie/Home.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Foodie
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnExit = new MetroFramework.Controls.MetroButton();
+            this.metroStyleManager1 = new MetroFramework.Components.MetroStyleManager(this.components);
+            this.bevragesTile = new MetroFramework.Controls.MetroTile();
+            this.metroToggle1 = new MetroFramework.Controls.MetroToggle();
+            this.appatizersTile = new MetroFramework.Controls.MetroTile();
+            this.EntreeTile = new MetroFramework.Controls.MetroTile();
+            this.dessertsTile = new MetroFramework.Controls.MetroTile();
+            this.saladsTile = new MetroFramework.Controls.MetroTile();
+            this.winecTiles = new MetroFramework.Controls.MetroTile();
+            this.kidsTile = new MetroFramework.Controls.MetroTile();
+            this.sidesTile = new MetroFramework.Controls.MetroTile();
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(23, 572);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(116, 34);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "EXIT";
+            this.btnExit.UseSelectable = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // metroStyleManager1
+            // 
+            this.metroStyleManager1.Owner = this;
+            this.metroStyleManager1.Style = MetroFramework.MetroColorStyle.Lime;
+            // 
+            // bevragesTile
+            // 
+            this.bevragesTile.ActiveControl = null;
+            this.bevragesTile.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bevragesTile.Location = new System.Drawing.Point(23, 101);
+            this.bevragesTile.Name = "bevragesTile";
+            this.bevragesTile.Size = new System.Drawing.Size(116, 40);
+            this.bevragesTile.TabIndex = 4;
+            this.bevragesTile.Text = "Bevrages";
+            this.bevragesTile.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.bevragesTile.UseSelectable = true;
+            this.bevragesTile.Click += new System.EventHandler(this.bevragesTile_Click);
+            // 
+            // metroToggle1
+            // 
+            this.metroToggle1.AutoSize = true;
+            this.metroToggle1.Location = new System.Drawing.Point(1197, 28);
+            this.metroToggle1.Name = "metroToggle1";
+            this.metroToggle1.Size = new System.Drawing.Size(80, 17);
+            this.metroToggle1.TabIndex = 5;
+            this.metroToggle1.Text = "Off";
+            this.metroToggle1.UseSelectable = true;
+            // 
+            // appatizersTile
+            // 
+            this.appatizersTile.ActiveControl = null;
+            this.appatizersTile.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.appatizersTile.Location = new System.Drawing.Point(23, 227);
+            this.appatizersTile.Name = "appatizersTile";
+            this.appatizersTile.Size = new System.Drawing.Size(116, 39);
+            this.appatizersTile.TabIndex = 6;
+            this.appatizersTile.Text = "Appatizers";
+            this.appatizersTile.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.appatizersTile.UseSelectable = true;
+            this.appatizersTile.Click += new System.EventHandler(this.appatizersTile_Click);
+            // 
+            // EntreeTile
+            // 
+            this.EntreeTile.ActiveControl = null;
+            this.EntreeTile.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.EntreeTile.Location = new System.Drawing.Point(23, 272);
+            this.EntreeTile.Name = "EntreeTile";
+            this.EntreeTile.Size = new System.Drawing.Size(116, 37);
+            this.EntreeTile.TabIndex = 7;
+            this.EntreeTile.Text = "Entree";
+            this.EntreeTile.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.EntreeTile.UseSelectable = true;
+            this.EntreeTile.Click += new System.EventHandler(this.EntreeTile_Click);
+            // 
+            // dessertsTile
+            // 
+            this.dessertsTile.ActiveControl = null;
+            this.dessertsTile.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.dessertsTile.Location = new System.Drawing.Point(23, 315);
+            this.dessertsTile.Name = "dessertsTile";
+            this.dessertsTile.Size = new System.Drawing.Size(116, 37);
+            this.dessertsTile.TabIndex = 8;
+            this.dessertsTile.Text = "Desserts";
+            this.dessertsTile.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.dessertsTile.UseSelectable = true;
+            this.dessertsTile.Click += new System.EventHandler(this.dessertsTile_Click);
+            // 
+            // saladsTile
+            // 
+            this.saladsTile.ActiveControl = null;
+            this.saladsTile.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.saladsTile.Location = new System.Drawing.Point(23, 358);
+            this.saladsTile.Name = "saladsTile";
+            this.saladsTile.Size = new System.Drawing.Size(116, 42);
+            this.saladsTile.TabIndex = 9;
+            this.saladsTile.Text = "Salads";
+            this.saladsTile.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.saladsTile.UseSelectable = true;
+            this.saladsTile.Click += new System.EventHandler(this.saladsTile_Click);
+            // 
+            // winecTiles
+            // 
+            this.winecTiles.ActiveControl = null;
+            this.winecTiles.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.winecTiles.Location = new System.Drawing.Point(23, 147);
+            this.winecTiles.Name = "winecTiles";
+            this.winecTiles.Size = new System.Drawing.Size(116, 38);
+            this.winecTiles.TabIndex = 10;
+            this.winecTiles.Text = "Wine/Cocktails";
+            this.winecTiles.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.winecTiles.UseSelectable = true;
+            this.winecTiles.Click += new System.EventHandler(this.winecTiles_Click);
+            // 
+            // kidsTile
+            // 
+            this.kidsTile.ActiveControl = null;
+            this.kidsTile.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.kidsTile.Location = new System.Drawing.Point(23, 453);
+            this.kidsTile.Name = "kidsTile";
+            this.kidsTile.Size = new System.Drawing.Size(116, 40);
+            this.kidsTile.TabIndex = 11;
+            this.kidsTile.Text = "Kids";
+            this.kidsTile.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.kidsTile.UseSelectable = true;
+            this.kidsTile.Click += new System.EventHandler(this.kidsTile_Click);
+            // 
+            // sidesTile
+            // 
+            this.sidesTile.ActiveControl = null;
+            this.sidesTile.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.sidesTile.Location = new System.Drawing.Point(23, 499);
+            this.sidesTile.Name = "sidesTile";
+            this.sidesTile.Size = new System.Drawing.Size(116, 37);
+            this.sidesTile.TabIndex = 12;
+            this.sidesTile.Text = "Sides";
+            this.sidesTile.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.sidesTile.UseSelectable = true;
+            this.sidesTile.Click += new System.EventHandler(this.sidesTile_Click);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1308, 629);
+            this.Controls.Add(this.sidesTile);
+            this.Controls.Add(this.kidsTile);
+            this.Controls.Add(this.winecTiles);
+            this.Controls.Add(this.saladsTile);
+            this.Controls.Add(this.dessertsTile);
+            this.Controls.Add(this.EntreeTile);
+            this.Controls.Add(this.appatizersTile);
+            this.Controls.Add(this.metroToggle1);
+            this.Controls.Add(this.bevragesTile);
+            this.Controls.Add(this.btnExit);
+            this.Name = "Home";
+            this.Text = "Foodie";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton btnExit;
+        private MetroFramework.Components.MetroStyleManager metroStyleManager1;
+        private MetroFramework.Controls.MetroTile bevragesTile;
+        private MetroFramework.Controls.MetroToggle metroToggle1;
+        private MetroFramework.Controls.MetroTile sidesTile;
+        private MetroFramework.Controls.MetroTile kidsTile;
+        private MetroFramework.Controls.MetroTile winecTiles;
+        private MetroFramework.Controls.MetroTile saladsTile;
+        private MetroFramework.Controls.MetroTile dessertsTile;
+        private MetroFramework.Controls.MetroTile EntreeTile;
+        private MetroFramework.Controls.MetroTile appatizersTile;
+    }
+}
+
---
+++ Foodie/Home.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace Foodie
+{
+    public partial class Home : MetroFramework.Forms.MetroForm
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            HomeSettings();
+        }
+
+        /*
+         * Initial settings for home screen of applicaiton 
+         * maximizes the windows form application
+         * set color of background 
+        */
+
+        private void HomeSettings()
+        {
+            WindowState = FormWindowState.Maximized;
+            Color color = ColorTranslator.FromHtml("#1a1a1a"); // this assumes an ARGB value 
+            this.BackColor = color;
+            this.CancelButton = this.btnExit;
+        }
+
+
+
+
+
+        /*
+         * MetroUI FrameWork tools implementations 
+         */
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+            this.BackColor = Color.Red;
+        }
+
+        private void bevragesTile_Click(object sender, EventArgs e)
+        {
+            using (Bevrages frm = new Bevrages())
+            {
+                frm.ShowDialog();
+            }
+        }
+
+        private void winecTiles_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void appatizersTile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EntreeTile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dessertsTile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saladsTile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kidsTile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sidesTile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        /*
+         * WinformUI common tools implementation  
+         */
+
+
+
+    }
+}
