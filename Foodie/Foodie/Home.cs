806418d5867d38a7b7cd5cdc1708266a6bbd8077
hamad Khawaja <khawajahamad217@gmail.com>
2017-04-08 16:43:12 -0400

    Initial Commit

---
+++ Foodie/Foodie/Home.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Foodie
+{
+    public partial class Home : MetroFramework.Forms.MetroForm
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
